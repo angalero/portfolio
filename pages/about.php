a09f95c16ccccb3c5d6c0f3e2f5e2b7249afeb00
angalero <agidaga@gmail.com>
2024-05-02 19:21:06 -0400

    added in settings for php

---
+++ pages/about.php
@@ -0,0 +1,14 @@
+<?php
+
+$tanx = array();
+	
+	$tanx[] = "<h2> Thanks for Joining! </h2>\n";
+	$tanx[] = "<p> Thanks for joining! Welcome to the family! We are so glad you 
+	decided to sign up. </p>\n";
+	$tanx[] = "<p> Now that you are all set up, Please sign into your accouunt, and
+	meet the mystical (and not so mystical) creatures in our library. Start checking out
+	books today!</p>\n";
+	$tanx[] = "<p> You can sign in here or put in your credentials on the top 
+	right corner of the screen. (**add arrow*)</p>\n";
+	
+	print(implode($tanx));
